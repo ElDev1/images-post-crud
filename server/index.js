9fe80068739ef433edab1e2e1ef8595b15ab349b
Devi Amaolo <deviamaolo@gmail.com>
2022-04-27 19:55:12 -0300

    setting controllers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import postRoutes from './routes/post.routes.js';
+
 const app = express();
 
+app.use(postRoutes);
 
 app.listen(3000)
 console.log('server running on port:', 3000);
