b49048f723093e406f377482418e31ade88feb35
Devi Amaolo <deviamaolo@gmail.com>
2022-05-02 01:10:55 -0300

    fixing errors and mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import postRoutes from './routes/post.routes.js';
+import { connectDB } from './db.js';
+import { PORT } from './config.js';
 
 const app = express();
+connectDB();
 
 app.use(postRoutes);
 
-app.listen(3000)
+app.listen(PORT)
 console.log('server running on port:', 3000);
