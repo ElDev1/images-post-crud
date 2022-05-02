8702168348df25e60dec933b2b69f94fa83b0e03
Devi Amaolo <deviamaolo@gmail.com>
2022-05-02 01:20:26 -0300

    post model

--- server/index.js
+++ server/index.js
@@ -1,12 +1,7 @@
-import express from 'express';
-import postRoutes from './routes/post.routes.js';
 import { connectDB } from './db.js';
 import { PORT } from './config.js';
+import app from './app.js';
 
-const app = express();
 connectDB();
-
-app.use(postRoutes);
-
 app.listen(PORT)
 console.log('server running on port:', 3000);
