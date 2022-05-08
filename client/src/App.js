5b79924a2066fac4a4e1ade5f7f101dd595762b1
Devi Amaolo <deviamaolo@gmail.com>
2022-05-08 19:57:00 -0300

    configuring tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,16 @@ import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Homepage/>} />
-      <Route path='/new' element={<PostForm/>} />
-      <Route path='*' element={<NotFoundPage/>} />
-    </Routes>
+    <div className='bg-neutral-900 min-h-screen flex items-center'>
+      <div className='px-10 container m-auto'>
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='/new' element={<PostForm/>} />
+          <Route path='*' element={<NotFoundPage/>} />
+        </Routes>
+      </div>
+    </div>
+    
   )
 }
 
