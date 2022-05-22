2644f1cfb9163b32a648b07ab8177017c38d8d4f
Devi Amaolo <deviamaolo@gmail.com>
2022-05-21 23:43:03 -0300

    adding navigation hook and error handling to the form

--- client/src/context/postContext.js
+++ client/src/context/postContext.js
@@ -19,7 +19,7 @@ export const PostProvider = ({children}) => {
 
     const createPost = async (post) => {
         const res = await createPostRequest(post)
-        console.log(res)
+        setPosts([...posts, res.data])
     }
 
     useEffect(() => {
--- client/src/pages/PostForm.js
+++ client/src/pages/PostForm.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {  Formik, Form, Field  } from 'formik'
+import {  Formik, Form, Field, ErrorMessage  } from 'formik'
 import { usePost } from '../context/postContext'
 import {  useNavigate } from 'react-router-dom'
+import * as Yup from 'yup'
 
 export const PostForm = () => {
 
@@ -14,14 +15,21 @@ export const PostForm = () => {
         title: '',
         description: ''
       }}
-      onSubmit={(values, actions) => {
-        createPost(values)
+      validationSchema={Yup.object({
+        title: Yup.string().required('title is required'),
+        description: Yup.string().required('description is required')
+      })}
+      onSubmit={async (values, actions) => {
+        await createPost(values)
+        navigate('/')
       }}
       >
         {({ handleSubmit }) => (
            <Form onSubmit={handleSubmit}>
-             <Field name="title" placeholder="title" />
-             <Field name="description" placeholder="description" />
+             <Field name="title" placeholder="title" className='px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full ' />
+             <ErrorMessage component='p' className='text-red-400 text-sm' name='title' />
+             <Field name="description" placeholder="description" className='px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full '/>
+             <ErrorMessage component='p' className='text-red-400 text-sm' name='description' />
              <button type="submit">Save</button>
            </Form>
         )}  
